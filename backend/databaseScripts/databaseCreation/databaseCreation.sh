d68a681c1d7169cf9ed03ea052f7921f1e5d670c
blakejameson <lpo13@txstate.edu>
2024-09-23 16:58:37 -0500

    CP-39 Added shebang to createTypesOfClothing because previously forgot
    Also added comments to the file on how to use this file

--- backend/databaseScripts/databaseCreation/databaseCreation.sh
+++ backend/databaseScripts/databaseCreation/databaseCreation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo mysql -u root -p -e "create database drobeApp" ;
+sudo mysql -u root -p -e "create database drobeApp"
+
+#Run the file using ./databaseCreation.sh
+
+#If you get a permissions denied error message, 
+# enter the linux command 'chmod +x databaseCreation.sh' 
+# which will make it executable and try running the script again.
+
