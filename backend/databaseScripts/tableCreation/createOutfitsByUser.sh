3174b989f8604554b202c40ae8f82a699084a441
Elham Islam <pzn14@txstate.edu>
2024-09-24 00:28:23 -0500

    CP-40 Created script for DB 'OutfitsByUser'
    (fixed ArticlesToImage file name)

---
+++ backend/databaseScripts/tableCreation/createOutfitsByUser.sh
@@ -0,0 +1,15 @@
+sudo mysql -u root -p -e "use drobeApp;
+    create table if not exists OutfitsByUser
+    ( 
+    outfitID INT AUTO_INCREMENT,
+    outfitName VARCHAR(100) NOT NULL,
+    userID INT NOT NULL,
+    timeCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timeLastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (outfitID) 
+    );
+    "
+
+
+# below are the columuns featured in this DB table
+#outfitID	outfitName	userID	timeCreated	timeLastModified
