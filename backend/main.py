c629f0b4da01cd4bf105736f6086918ae4e1eaf0
blakejameson <lpo13@txstate.edu>
2024-09-25 23:23:01 -0500

    Loading in variables from .env file.
    Using values from env file to be able to create the connection to the database. Also developed a model that defines the fields in the request body sent by the UI. developing endpoint to create clothing article

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,48 @@
 # Use the following link as reference for initial API setup:
 # https://fastapi.tiangolo.com/tutorial/first-steps/
 
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic import BaseModel
+import mysql.connector
+
+load_dotenv()
+
+databaseHost = os.getenv('host')
+databaseUser = os.getenv('user')
+databasePassword = os.getenv('password')
+
+
+drobeDatabaseConnection = mysql.connector.connect(
+    host = databaseHost,
+    user = databaseUser,
+    password= databasePassword,
+    database = 'drobeApp'
+)
+
+print(drobeDatabaseConnection)
 
 app = FastAPI()
+
+class clothingArticle(BaseModel):
+    clothingTypeID: int
+    clothingType: str
+    clothingArticleName: str
+    userID: int
 
 
 @app.get("/")
 async def root():
     return {"message": "This is the root endpoint AKA path AKA route"}
+
+
+@app.post("/createArticleOfClothing")
+async def createArticleOfClothing(clothingArticle: clothingArticle):
+    clothingType = clothingArticle.clothingType
+    clothingTypeID = clothingArticle.clothingTypeID
+    clothingArticleName = clothingArticle.clothingArticleName
+    userID = clothingArticle.userID
+    print(clothingArticle)
+
+
