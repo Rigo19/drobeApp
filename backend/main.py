0b427ae8b77a260ba0e35af74442a7aa47d06d86
blakejameson <lpo13@txstate.edu>
2024-09-26 00:31:03 -0500

    Added functionality to save new clothing article to database

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 import mysql.connector
+from datetime import datetime
+
+
 
 load_dotenv()
 
@@ -21,15 +24,29 @@ drobeDatabaseConnection = mysql.connector.connect(
     database = 'drobeApp'
 )
 
-print(drobeDatabaseConnection)
+
+# the cursor defined below is what you will 
+# use to run queries with the database
+
+drobeDatabaseCursor = drobeDatabaseConnection.cursor()
+
 
 app = FastAPI()
+
+# this clothingArticle model below represents the fields that 
+# will be received in the Request body sent from the UI
 
 class clothingArticle(BaseModel):
     clothingTypeID: int
     clothingType: str
     clothingArticleName: str
     userID: int
+
+
+createArticleOfClothing_SQL_Query = ("INSERT INTO ArticlesOfClothing "
+                                     "(clothingTypeID, clothingType, clothingArticleName, userID, timeAdded, numberOfOutfitsAssociatedWith) "
+                                     "VALUES (%s, %s, %s, %s, %s, %s)")
+
 
 
 @app.get("/")
@@ -39,10 +56,27 @@ async def root():
 
 @app.post("/createArticleOfClothing")
 async def createArticleOfClothing(clothingArticle: clothingArticle):
+    #line below creates the timestamp that will be stored in database
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     clothingType = clothingArticle.clothingType
     clothingTypeID = clothingArticle.clothingTypeID
     clothingArticleName = clothingArticle.clothingArticleName
     userID = clothingArticle.userID
-    print(clothingArticle)
+    numberOfOutfitsAssociatedWith = 0
+
+    #I highly recommend the link below to see official documentation on how mysql connector uses the execute function below
+    # https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
+
+    #Essentially here, I am lining up the values to be inserted into the ArticlesOfClothing database table. This is used in 
+    # conjunction with the 'createArticleOfClothing_SQL_Query' query defined higher in the program
+
+    clothing_data = (clothingTypeID, clothingType, clothingArticleName, userID, timestamp, numberOfOutfitsAssociatedWith)
+
+    drobeDatabaseCursor.execute(createArticleOfClothing_SQL_Query, clothing_data)
+    #print(clothingArticle)
+
+    drobeDatabaseConnection.commit()
+
+    return clothingArticle
 
 
