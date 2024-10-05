52f12b61178dce54e9b49ece8b4ae3bac56c19a2
mnt43 <mnt43@txstate.edu>
2024-10-04 20:17:42 -0500

    CP-88 updated whole setup proccess
    to accommodate the new sh files added and edited

--- backend/databaseScripts/wholeSetupProcess.sh
+++ backend/databaseScripts/wholeSetupProcess.sh
@@ -2,8 +2,8 @@
 chmod +x ./databaseCreation/databaseCreation.sh
 chmod +x ./tableCreation/createArticlesToImage.sh
 chmod +x ./tableCreation/createArticlesOfClothing.sh
-chmod +x ./tableCreation/createArticlesToOutfits.sh
-chmod +x ./tableCreation/createOutfitsByUser.sh
+chmod +x ./tableCreation/createOutfitItems.sh
+chmod +x ./tableCreation/createOutfits.sh
 chmod +x ./tableCreation/createTypesOfClothing.sh
 chmod +x ./tableCreation/createUserInfo.sh
 chmod +x ./tableCreation/createWebSessions.sh
@@ -12,10 +12,8 @@ chmod +x ./tableCreation/createWebSessions.sh
 ./databaseCreation/databaseCreation.sh
 ./tableCreation/createArticlesToImage.sh
 ./tableCreation/createArticlesOfClothing.sh
-./tableCreation/createArticlesToOutfits.sh
-./tableCreation/createOutfitsByUser.sh
+./tableCreation/createOutfitItems.sh
+./tableCreation/createOutfits.sh
 ./tableCreation/createTypesOfClothing.sh
 ./tableCreation/createUserInfo.sh
 ./tableCreation/createWebSessions.sh
-
-
