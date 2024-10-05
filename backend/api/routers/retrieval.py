a07f5b2e56b36df6d41b5adc867eb0412b250e25
blakejameson <lpo13@txstate.edu>
2024-10-05 15:32:18 -0500

    CP-59 continuing to reconfigure files for organization

---
+++ backend/api/routers/retrieval.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel
+
+
+
+#Following code would return all clothing articles for a user.
+@app.get("/getAllClothingArticlesByUserID/{userID}")
+async def get_all_clothing_articles_by_userID(userID: int):
+    retrieve_articles_by_userID_query = "SELECT * FROM ArticlesOfClothing WHERE userID = %s"
+    
+    drobeDatabaseCursor.execute(retrieve_articles_by_userID_query, (userID,))
+    articles = drobeDatabaseCursor.fetchall()
+
+    if not articles:
+        return {"message": "No articles found in the user's closet"}
+    
+    result = []
+    for article in articles:
+        result.append({
+            "clothingArticleID": article[0],
+            "clothingTypeID": article[1],
+            "clothingType": article[2],
+            "clothingArticleName": article[3],
+            "userID": article[4],
+            "timeAdded": article[5],
+            "numberOfOutfitsAssociatedWith": article[6]
+        })
+
+    return {"message": result}
+
+#Following would return the image/images for a single article of clothing
+@app.get("/get_images_for_clothing_article/{clothingArticleID}")
+async def get_images_for_clothing_article(clothingArticleID: int):
+    retrieve_images_query = "SELECT Image FROM ArticlesToImage WHERE clothingArticleID = %s"
+    
+    drobeDatabaseCursor.execute(retrieve_images_query, (clothingArticleID,))
+    images = drobeDatabaseCursor.fetchall()
+
+    if not images:
+        return {"message": "No images found for this clothing article"}
+    
+    firstImage = images[0][0]
+
+    return Response(content = firstImage, media_type = "image")
+
+#Following code could be useful in testing frontend (Probably won't be used in final code)
+@app.get("/getAllClothingArticles/")
+async def get_all_clothing_articles():
+    retrieve_all_articles_query = "SELECT * FROM ArticlesOfClothing"
+    
+    drobeDatabaseCursor.execute(retrieve_all_articles_query)
+    articles = drobeDatabaseCursor.fetchall()
+
+    if not articles:
+        return {"message": "No articles found in closet"}
+    
+    result = []
+    for article in articles:
+        result.append({
+            "clothingArticleID": article[0],
+            "clothingTypeID": article[1],
+            "clothingType": article[2],
+            "clothingArticleName": article[3],
+            "userID": article[4],
+            "timeAdded": article[5],
+            "numberOfOutfitsAssociatedWith": article[6]
+        })
+
+    return {"message": result}
