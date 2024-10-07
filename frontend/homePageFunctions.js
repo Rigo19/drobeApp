7fdcec937c202fa562d71d6cea493550961743ad
blakejameson <lpo13@txstate.edu>
2024-10-06 21:16:34 -0500

    CP-95 implemented functionality to show upto 6 clothing articles on home

--- frontend/homePageFunctions.js
+++ frontend/homePageFunctions.js
@@ -1,4 +1,6 @@
 var getUserClothingArticles_URL = "http://127.0.0.1:8000/getAllClothingArticlesByUserID/"
+var getImageOfArticle = "http://127.0.0.1:8000/get_images_for_clothing_article/";
+
 var articles = []
 
 //get(fetcht) all the clothing pieces from the database of the specific user
@@ -35,6 +37,44 @@ async function getImageFile(current_article_ID){
 
 async function main(){
     await getAllArticlesOfClothing()
+
+    var mainClosetDiv = document.getElementById('mycloset-div')
+
+    for (var i = 0; i < 6; i++){
+
+        var currentElement = articles[i];
+        var clothingArticleID = currentElement.clothingArticleID;
+        var clothingArticleName = currentElement.clothingArticleName;
+        var currentImageFile = await getImageFile(clothingArticleID)
+        
+        var mainArticleDiv = document.createElement('div');
+        mainArticleDiv.className = 'col-md-4';
+
+        var firstInnerArticleDiv = document.createElement('div')
+        firstInnerArticleDiv.className = 'card mb-4 shadow-sm';
+
+        var imageElement = document.createElement('img')
+        imageElement.className = 'card-img-top';
+        var imageElementSrc = URL.createObjectURL(currentImageFile);
+
+        imageElement.src = imageElementSrc;
+        firstInnerArticleDiv.append(imageElement);
+
+
+        var mostInnerArticleDiv = document.createElement('div')
+        mostInnerArticleDiv.className = 'card-body text-center';
+        
+        var card = document.createElement('h5');
+        card.innerText = clothingArticleName;
+
+        mostInnerArticleDiv.append(card);
+
+        firstInnerArticleDiv.append(mostInnerArticleDiv);
+
+        mainArticleDiv.append(firstInnerArticleDiv);
+        mainClosetDiv.append(mainArticleDiv);
+
+    }   
 }
 
 main()
